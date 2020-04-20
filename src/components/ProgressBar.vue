146e73d001a51f98d7e22ce0903cce7e36175b88
Airop <janolenielsen99@gmail.com>
2020-04-21 01:56:58 +0200

    -added a progress bar to give visual feedback to the user while data is being pulled

--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -1,6 +1,19 @@
 <template>
-<v-progress-linear
-  indeterminate=$store.state.loading
-  color="red"
-></v-progress-linear>
+  <v-progress-linear
+    :indeterminate="$store.state.loading"
+    :active="$store.state.loading"
+    color="red"
+  ></v-progress-linear>
 </template>
+
+<script>
+export default {
+  methods: {
+    getLoading(state) {
+      return state.loading;
+    }
+  }
+};
+</script>
+
+<style scoped></style>
