a71be710e9e9219720287d7bac528776399f80b5
emrearkan <emre-arkan@outlook.com>
2020-05-22 16:18:40 +0200

    add close and watch buttons to overlay remove unused variables

--- src/components/LectureCard.vue
+++ src/components/LectureCard.vue
@@ -1,14 +1,13 @@
 <template>
   <v-hover v-slot:default="{ hover }">
-    <v-card class="text-xs-center ma-2" @mouseleave="flipped = false">
+    <v-card class="text-xs-center ma-2">
       <v-fade-transition>
         <v-overlay v-if="hover" absolute color="grey" z-index="0" opacity="0">
           <v-btn
-            x-large
             color="red darken-4"
             :to="createLinToVideoLecture(videoLecture.videoLecture.value)"
           >
-            <v-icon>mdi-arrow-right</v-icon>
+            <v-icon>ondemand_video</v-icon>
           </v-btn>
         </v-overlay>
       </v-fade-transition>
@@ -16,20 +15,18 @@
         :src="require(`@/assets/logos/${videoLecture.thumbnailName.value}`)"
         height="16vh"
         contain
-        v-if="!flipped"
       >
       </v-img>
       <v-card-title>
         <div class="heading">{{ videoLecture.label.value }}</div>
       </v-card-title>
       <v-card-text class="hyphens text-justify">
-        <v-clamp autoresize :max-lines="4" :expanded="flipped">
+        <v-clamp autoresize :max-lines="4">
           {{ videoLecture.description.value }}
         </v-clamp>
       </v-card-text>
-      <v-overlay :value="overlay" v-if="hover">
+      <v-overlay :value="overlay" z-index="1" opacity="0.75">
         <v-card
-          v-on:click="overlay = !overlay"
           class="text-xs-center ma-2"
           max-width="400"
           color="white"
@@ -39,7 +36,6 @@
             :src="require(`@/assets/logos/${videoLecture.thumbnailName.value}`)"
             height="16vh"
             contain
-            v-if="!flipped"
           >
           </v-img>
           <v-card-title>
@@ -48,25 +44,33 @@
             </div>
           </v-card-title>
           <v-card-text class="hyphens text-justify black--text">
-            <v-clamp autoresize :max-lines="4" :expanded="overlay">
+            <v-clamp autoresize>
               {{ videoLecture.description.value }}
             </v-clamp>
           </v-card-text>
+          <v-card-actions z-index="1">
+            <v-row align="center" justify="center">
+              <v-btn icon="close" color="black" v-on:click="overlay = !overlay">
+                <v-icon large dark>close</v-icon>
+              </v-btn>
+              <v-btn
+                icon="close"
+                color="black"
+                :to="createLinToVideoLecture(videoLecture.videoLecture.value)"
+              >
+                <v-icon large dark>ondemand_video</v-icon>
+              </v-btn>
+            </v-row>
+          </v-card-actions>
         </v-card>
       </v-overlay>
 
       <v-card-actions z-index="1">
         <v-list-item class="grow">
           <v-row align="center" justify="space-around">
-            <v-btn
-              text
-              color="deep-purple accent-4"
-              v-on:click.prevent
-              v-on:click="overlay = !overlay"
-            >
+            <v-btn text color="red darken-4" v-on:click="overlay = !overlay">
               More
             </v-btn>
-
             <v-spacer></v-spacer>
             <v-card-subtitle></v-card-subtitle>
           </v-row>
@@ -86,17 +90,13 @@ export default {
   },
   data() {
     return {
-      overlay: false,
-      flipped: false
+      overlay: false
     };
   },
   components: {
     VClamp
   },
   methods: {
-    flip() {
-      this.flipped = !this.flipped;
-    },
     createLinToVideoLecture(videoLectureIRI) {
       const dashIndex = videoLectureIRI.search('#') + 1;
       return videoLectureIRI.substring(dashIndex, videoLectureIRI.length);
