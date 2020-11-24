a1b0c24346bfc978b30faf59d3af7cef7fa49eb2
Wenxin Hu <happyhwx888@googlemail.com>
2020-11-24 14:19:59 +0100

    video page design with embedded video

--- src/views/Video.vue
+++ src/views/Video.vue
@@ -1,15 +1,63 @@
 <template>
-  <div>
-    <v-img
-      :src="require(`@/assets/logos/${videoLecture.thumbnail}`)"
-      height="16vh"
-      contain
-    >
-    </v-img>
-    <h1>{{ videoLecture.name }}</h1>
-    <div>
+  <div class="grey lighten-4">
+    <v-card class="mx-auto white" elevation="3" max-width="80%" outlined>
+      <v-system-bar color="#AE001C">
+        <v-spacer></v-spacer>
+      </v-system-bar>
+      <v-card-title class="display-1 text--primary">{{
+        videoLecture.headline
+      }}</v-card-title>
+      <!--<v-img height="700" src="../assets/fbw_image.jpg"></v-img>-->
+      <video-player configuration='{
+      "streams": [{"hd": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"}]
+    }'></video-player>
+
+      <v-container>
+        <v-row no-gutters>
+          <v-col :key="1" cols="12" sm="2">
+            <v-img
+              :src="require(`@/assets/logos/${videoLecture.thumbnail}`)"
+              class="white--text align-end"
+            >
+            </v-img>
+          </v-col>
+          <v-col :key="2" cols="12" sm="10">
+            <v-card-text>
+              {{ videoLecture.description }}
+            </v-card-text>
+          </v-col>
+        </v-row>
+      </v-container>
+      <!--<v-card-text class="text-md-body-1 font-weight-medium text&#45;&#45;primary">
       {{ videoLecture.description }}
-    </div>
+    </v-card-text>-->
+      <!--<div>
+     <video-player configuration='{
+      "streams": [{"hd": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"}]
+    }'></video-player>
+   </div>-->
+    </v-card>
+
+    <v-container width="80vh">
+      <v-row no-gutters>
+        <v-col v-for="(n, index) in lectures" :key="index" cols="12" sm="3">
+          <v-card max-width="90%">
+            <v-row align="center">
+              <v-col cols="6" sm="2">
+                <v-card-title class="display-1">
+                  {{ index + 1 }}
+                </v-card-title>
+              </v-col>
+              <v-col cols="6" sm="4">
+                <v-card-text class="text--primary text-lg-body-1 font-weight-medium">
+                  {{ n }}
+                </v-card-text>
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
@@ -22,12 +70,27 @@ export default {
   data() {
     return {
       videoLectureIri: null,
-      videoLecture: null
+      videoLecture: null,
+      lectures: ['lecture 01', 'lecture 02', 'lecture 03', 'lecture 04']
     };
   },
   created() {
     this.videoLectureIri = this.$route.params.id;
     this.getVideoLectureDetails();
+  },
+  mounted() {
+    const pluginVideoPlayer = document.createElement('script');
+    pluginVideoPlayer.setAttribute('src', '../js/video-player.js');
+    pluginVideoPlayer.async = true;
+    document.head.appendChild(pluginVideoPlayer);
+
+    const pluginVideoWebComponent = document.createElement('script');
+    pluginVideoWebComponent.setAttribute(
+      'src',
+      '../js/webcomponents-bundle.js'
+    );
+    pluginVideoWebComponent.async = true;
+    document.head.appendChild(pluginVideoWebComponent);
   },
   methods: {
     async getVideoLectureDetails() {
