cf5d327ed5b17861363f76a43b2c832b28705864
Wenxin Hu <happyhwx888@googlemail.com>
2021-03-12 16:04:51 +0100

    title change to corresponding language tag

--- src/components/THBImage.vue
+++ src/components/THBImage.vue
@@ -5,7 +5,7 @@
     <v-row align="center" justify="center">
       <v-card color="rgb(255,255,255,0.6)">
         <v-col class="text-center" cols="12">
-          <h1 class="display-3 font-weight-bold mb-4">Video lectures</h1>
+          <h1 class="display-3 font-weight-bold mb-4">{{ $t('page.title') }}</h1>
           <h4 class="display-1">
             {{ $t('page.description') }}
           </h4>
