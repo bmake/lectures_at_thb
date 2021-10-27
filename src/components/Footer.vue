d53688e1eac4457f6f284baf6567ac4385ec682f
Wenxin Hu <happyhwx888@googlemail.com>
2021-10-27 23:36:47 +0200

    routing to absolute path

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,12 +2,13 @@
   <v-footer>
     <v-col class="text-center" cols="12">
       <strong>&copy; {{ $t('page.description') }}</strong> -
-      {{ new Date().getFullYear() }} &bull; <router-link to="/impressum">{{ $t('impressum.title') }}</router-link> &bull; <router-link to="/privacy">{{ $t('privacy.title') }}</router-link>
+      {{ new Date().getFullYear() }} &bull; <router-link :to="{ name: 'impressum' }">{{ $t('impressum.title') }}</router-link> &bull; <router-link :to="{ name: 'privacy' }">{{ $t('privacy.title') }}</router-link>
     </v-col>
   </v-footer>
 </template>
 
 <script>
+
 export default {
   name: 'Footer'
 };
--- src/views/Privacy.vue
+++ src/views/Privacy.vue
@@ -10,13 +10,13 @@
     </v-card>
     <div style="padding: 2em; padding-left: 4em; padding-right: 4em">
       <v-row class="font-weight-bold" style="padding: 1em">{{ $t('privacy.subtitle') }}</v-row>
-      <v-row v-for="(t, i) in $t('address')" v-bind:key="i" style="padding: 0.2em; padding-left: 1em">{{ t }}</v-row>
+      <v-row v-for="(t, i) in $t('address')" v-bind:key="'address' + i" style="padding: 0.2em; padding-left: 1em">{{ t }}</v-row>
       <v-row class="font-weight-bold" style="padding: 1em">{{ $t('privacy.dataprotection.title') }}</v-row>
-      <v-row v-for="(t, i) in $t('privacy.dataprotection.text')" v-bind:key="i" style="padding: 0.2em; padding-left: 1em; padding-right: 1em">{{ t }}</v-row>
+      <v-row v-for="(t, i) in $t('privacy.dataprotection.text')" v-bind:key="'dataprotection' + i" style="padding: 0.2em; padding-left: 1em; padding-right: 1em">{{ t }}</v-row>
       <v-row class="font-weight-bold" style="padding: 1em">{{ $t('privacy.InfDelBlo.title') }}</v-row>
       <v-row style="padding-left: 1em; padding-right: 1em">{{ $t('privacy.InfDelBlo.text') }}</v-row>
       <v-row class="font-weight-bold" style="padding: 1em">{{ $t('privacy.log.title') }}</v-row>
-      <v-row v-for="(t, i) in $t('privacy.log.text')" v-bind:key="i" style="padding: 0.2em; padding-left: 1em; padding-right: 1em">{{ t }}</v-row>
+      <v-row v-for="(t, i) in $t('privacy.log.text')" v-bind:key="'log'+ i" style="padding: 0.2em; padding-left: 1em; padding-right: 1em">{{ t }}</v-row>
       <v-row class="font-weight-bold" style="padding: 1em">{{ $t('privacy.cookie.title') }}</v-row>
       <v-row style="padding-left: 1em; padding-right: 1em">{{ $t('privacy.cookie.text') }}</v-row>
       <v-row class="font-weight-bold" style="padding: 1em">{{ $t('privacy.changeData.title') }}</v-row>
