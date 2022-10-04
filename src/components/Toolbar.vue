7d0a11162a43a50ccd1ce7ba58fcf8948f4932ca
Wenxin Hu <happyhwx888@googlemail.com>
2022-10-04 17:27:01 +0200

    comment search icon / function

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -47,9 +47,9 @@
         </v-menu>
       </div>
 
-      <v-btn icon disabled>
+      <!--<v-btn icon disabled>
         <v-icon>mdi-magnify</v-icon>
-      </v-btn>
+      </v-btn>-->
     </v-toolbar>
   </div>
 </template>
